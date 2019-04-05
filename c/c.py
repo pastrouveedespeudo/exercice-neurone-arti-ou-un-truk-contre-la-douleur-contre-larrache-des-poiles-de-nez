55500a2cbcbcef950a1d81169adfa4e25790ecaf
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 23:55:15 +0200

    Add files via upload

---
+++ c/c.py
@@ -0,0 +1,39 @@
+exécutant = ["serieux", "calme", "sécurité", "responsable",
+             "fiable", "concentré", "organisé"]
+
+gardien = ["pratique", "organisé", "athlétique", "loyal",
+           "responsable", "travailleur", "bon citoyen"]
+
+infirmier = ["calme", "gentil", "consciencieux", "stable",
+             "pratique", "observateur", "besoin des autres avant les siens"]
+
+aide à domicile = ["chaleureux", "populaire", "consciencieux",
+                   "responsable", "besoin de gratitude"]
+
+mécanicien = ["calme", "reservé", "interessé par comment ca marche",
+              "raisonnement", "risque", "sportif"]
+
+homme_action = ["action", "impulsif", "fidele", "respectueux"]
+
+acteur = ["ouvert", "extravertit"]
+
+artiste = ["calme", "serieux", "sensible", "gentil", "loyal", "fidele",
+           "flexible"]
+
+directeur = ["Énergique","franc","compréhension",
+             "Intelligent","impatient"]
+
+scientifique = ["Indépendant","original","analytique ","déterminé"]
+
+visionnaire = ["Créatif","débrouillard","intellectuellement rapide"
+               ,"débattre","franc","affirmé"]
+
+penseur = ["Logique","original ","créatif","logique","Calme ","réservé","Individualiste"]
+
+donateur = ["Populaire ","sensible","sociable"]
+
+protecteur = ["Original","sensible","intuitif"]
+
+charismatique = ["Enthousiaste","idéaliste","créatif","ouvert"]
+
+idéaliste = ["Calme","réfléchi ","idéaliste","fidèle","écrivain"]
