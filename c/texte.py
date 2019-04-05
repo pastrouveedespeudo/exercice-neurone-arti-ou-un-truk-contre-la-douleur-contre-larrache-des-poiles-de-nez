781ea7a6abdbb61b7d63ab257ea8adc392603a02
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 23:53:50 +0200

    Add files via upload

---
+++ c/texte.py
@@ -0,0 +1,1 @@
+texte = "Cher Maxime, Je suis très heureux que tu viennes. Je suis sur que nous allons faire de la marche en montagne.Mélanie vient avec nous dans les Vosges.  Il faut que tu emportes des chaussures de marche, une lampe de poche, une boussole et un K-way, en cas de pluie..Mes amitiés, Baptiste "
