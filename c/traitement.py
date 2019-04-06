627c7f7f27470062a80209d55698c39f9c4d9abd
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-06 23:07:25 +0200

    Update traitement.py

--- c/traitement.py
+++ c/traitement.py
@@ -69,6 +69,10 @@ class poidsPonctuation:
 
 class poidsTexte:
 
+    def type_verbe(self, texte):
+        pass
+    #transitif ou non
+
     def longueurTexte(self, texte):
         self.texte = texte
         longueur = len(self.texte)
@@ -80,6 +84,115 @@ class poidsTexte:
 
     def structure_phrase(self, texte):
         self.texte = texte
+        #on cherche chaque mot (sujet, nom ect)
+        #on cherche le verbe avant
+
+        liste = [[],[],[],[],[],[],[],[],[],[],[],[],
+                 [],[],[],[],[],[],[],[],[],[],[],[],
+                 [],[],[],[],[],[],[],[],[],[],[],[],
+                 [],[],[],[],[],[],[],[],[],[],[],[]]
+
+        c = 0
+        for i in self.texte:
+            for j in i:
+                
+                path = "http://www.les-verbes.com/conjuguer.php?verbe={}"
+                path = path.format(j.lower())
+                requete = requests.get(path)
+                page = requete.content
+
+                soup = BeautifulSoup(page, "html.parser")      
+                propriete = soup.find_all("span", {"class":"arial-12-gris"})
+                verbe = str(propriete).find("VERBE")
+                propriete = soup.find_all("span", {"class":"arial-12-gris"})
+                if verbe >= 0:
+                    liste[c].append("verbe")
+                else:
+                    path = "https://www.le-dictionnaire.com/definition/{}"
+                    path = path.format(j.lower())
+                    requete = requests.get(path)
+                    page = requete.content
+                    soup = BeautifulSoup(page, "html.parser")      
+                    propriete = soup.find("div", {"class":"defbox"})
+                    
+                    pronom_personnel = str(propriete).find("Pronom personnel")
+                    nom_commun = str(propriete).find("Nom commun")
+                    adjectif = str(propriete).find("Adjectif")
+                    conjonction_de_coordination = str(propriete).find("Conjonction de coordination")
+                    adverbe = str(propriete).find("Adverbe")
+                    adjectif_possessif = str(propriete).find("Adjectif possessif")
+                    forme_article_défini = str(propriete).find("Forme d’article défini")
+                    Forme_d_article_défini = str(propriete).find("Forme d’article défini")
+                    article_défini = str(propriete).find("Article défini")
+                    adjectif_numéral = str(propriete).find("Adjectif numéral")
+                    forme_de_nom_commun = str(propriete).find("Forme de nom commun")
+                    préposition = str(propriete).find("Préposition")
+                    conjonction = str(propriete).find("Conjonction")
+                    Forme_article_indéfini = str(propriete).find("Forme d’article indéfini")
+                    Forme_adjectif_numéral = str(propriete).find("Forme d’adjectif numéral")
+                    
+                    Aucun_mot_trouvé  = str(propriete).find("Aucun mot trouvé, donc aucune définition")
+                    
+                    if pronom_personnel >= 0:
+                        liste[c].append("pronom_personnel")
+                    
+                    elif nom_commun >= 0:
+                        liste[c].append("nom_commun")
+                    
+                    elif adjectif >= 0:
+                        liste[c].append("adjectif")
+                        
+                    elif conjonction_de_coordination >= 0:
+                        liste[c].append("conjonction_de_coordination")
+                
+                    elif adverbe >= 0:
+                        liste[c].append("adverbe")
+                    
+                    elif adjectif_possessif >= 0:
+                        liste[c].append("adjectif_possessif")
+                    
+                    elif forme_article_défini >= 0:
+                        liste[c].append("forme_article_défini")
+                    
+                    elif Forme_d_article_défini >= 0:
+                        liste[c].append("Forme_d_article_défini")
+                    
+                    elif article_défini >= 0:
+                        liste[c].append("article_défini")
+                    
+                    elif adjectif_numéral >= 0:
+                        liste[c].append("adjectif_numéral")
+                    
+                    elif forme_de_nom_commun >= 0:
+                        liste[c].append("forme_de_nom_commun")
+                        
+                    elif préposition >= 0:
+                        liste[c].append("préposition")
+                    
+                    elif conjonction >= 0:
+                        liste[c].append("conjonction")
+                        
+                    elif Forme_article_indéfini >= 0 :
+                        liste[c].append("Forme_article_indéfini")
+
+                    elif Forme_adjectif_numéral >= 0:
+                        liste[c].append("Forme_adjectif_numéral")
+                        
+                    elif Aucun_mot_trouvé >= 0:
+                        liste[c].append("Aucun_mot_trouvé")
+
+                    else:
+                        print(j, "mot a chercher !!!")
+            
+                    
+
+                                    
+                                
+            c+=1
+        return liste
+        #a noter que maxime == maximiser tu pourrais comparer le mot avec le mot trouver
+        #dans le dico comme pour les fautes etdes fois ca fait pas lower...
+
 
     def impératif(self, texte):
         self.texte = texte
@@ -182,8 +295,9 @@ if __name__ == "__main__":
     txt = premiere_opération.trait_texte(TEXT)
     text1 = premiere_opération.traitement_phrase(txt)
 
+    
     poidsTexte = poidsTexte()
-    poidsTexte.impératif(text1)
+    poidsTexte.structure_phrase(text1)
 
 
 
