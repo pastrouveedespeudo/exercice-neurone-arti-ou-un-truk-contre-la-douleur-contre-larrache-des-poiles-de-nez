8fcd04d7627a39839eecbe9bc9399fea79a19817
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-06 22:04:38 +0200

    Update conteneur.py

--- c/conteneur.py
+++ c/conteneur.py
@@ -1,9 +1,12 @@
+
+
 PRONOMS = ["je","tu","il","nous","vous","ils"]
 
 FEMININ = ["lle","nne","tte","sse","ve","se","che","gne","ée",
            "ées"]
 
-
+IMPERATIF = ["e", "ons", "ds", "is","issons", "issez", "is", "ons"]
 
         
+
 
