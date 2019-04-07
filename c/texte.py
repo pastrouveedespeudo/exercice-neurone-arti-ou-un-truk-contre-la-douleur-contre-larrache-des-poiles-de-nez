250d2497c5185dc5ffc8d290c7f1e0866dfdf428
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-07 14:35:39 +0200

    Update texte.py

--- c/texte.py
+++ c/texte.py
@@ -27,19 +27,19 @@ TEXT2 = "Salut Mehdi\
         À bientôt.\
         Yassine"
 
-TEXTE3 = "Depuis deux mois, les petits renards sont nés. Alors, commence pour le\
+TEXT3 = "Depuis deux mois, les petits renards sont nés. Alors, commence pour le\
         père une vie dure et dangereuse. Au petit jour, il quitte sa retraite et passe la rivière pour venir rôder\
         près d'une maison isolée, au milieu d'une sapinière. Caché dans les fougères, il attend patiemment le\
         passage des volailles imprudentes."
 
-TEXTE4 =  "Élisabeth vit partir Jacques, les jambes raides, le dos rond, les coudes au corps,\
+TEXT4 =  "Élisabeth vit partir Jacques, les jambes raides, le dos rond, les coudes au corps,\
             dans une attitude crispée. Il tomba dans un virage, se releva et continua sa descente plus lentement.\
             Élisabeth était meilleure skieuse que lui. Elle s'engagea résolument dans le chemin poli et durci par\
             des centaines de passages."
 
 
 
-TEXTE5 = "Je m'étais assis contre le mur de pierres sèches qui borde le pré. Le soleil de juillet\
+TEXT5 = "Je m'étais assis contre le mur de pierres sèches qui borde le pré. Le soleil de juillet\
         tombant à pic m'écrasait d'une sorte de torpeur, lorsque je vis la vipère. Je ne l'avais pas entendue\
         arriver. Elle avait dû longer le pied du mur et, rencontrant cet obstacle que je représentais pour elle,\
         elle s'était arrêtée. Trouvant sans doute l'endroit propice à un bain de soleil, elle s'était lovée à\
@@ -48,7 +48,7 @@ TEXTE5 = "Je m'étais assis contre le mur de pierres sèches qui borde le pré. 
 
 
 
-TEXTE6 = "Je suis seule, lisant tes chères petites lettres avec les larmes aux\
+TEXT6 = "Je suis seule, lisant tes chères petites lettres avec les larmes aux\
         yeux. Dans une quinzaine de jours, je vous reverrai, je vous embrasserai ; nous en aurons pour\
         longtemps à être ensemble, et je serai bien heureuse. Continue d'être bonne et douce et de faire ma\
         joie ; sois attentive et tendre avec ton excellente mère. Elle vous aime tant et est si digne d'être\
@@ -56,7 +56,7 @@ TEXTE6 = "Je suis seule, lisant tes chères petites lettres avec les larmes aux\
         toi. Je vois avec plaisir que tu aimes et que tu comprends la nature. "
 
 
-TEXTE7 =  "J'ai retournée ma chaise et je l'ai placée comme celle du marchand de tabac\
+TEXT7 =  "J'ai retournée ma chaise et je l'ai placée comme celle du marchand de tabac\
             parce que j'ai trouvée que c'était plus commode. J'ai fumée deux cigarettes, je suis rentré pour prendre\
             un morceau de chocolat et je suis revenu le manger à la fenêtre. Peu après, le ciel s'est assombri et\
             j'ai cru que nous allions avoir un orage. Il s'est découvert peu à peu cependant. Mais le passage des\
@@ -64,22 +64,67 @@ TEXTE7 =  "J'ai retournée ma chaise et je l'ai placée comme celle du marchand 
             ciel. À cinq heures, des tramways sont arrivés dans le bruit"
 
 
-TEXTE8 = "je suis une petite fille et j'adore les vetements. J'étais seule devant\
+TEXT8 = "je suis une petite fille et j'adore les vetements. J'étais seule devant\
         mon mirroir et je me vis si belle."
 
-TEXTE9 = "le soir quand je suis belle j'aime me levée et m'assoire"
+TEXT9 = "le soir quand je suis belle j'aime me levée et m'assoire"
 
-TEXTE10 = "Momo je t'embrasse. je suis contente de pouvoir t'ecrire au bordel\
+TEXT10 = "Momo je t'embrasse. je suis contente de pouvoir t'ecrire au bordel\
         que c'ets chiant. Mais je suis chiante. j'aime mon doudou et faire les magasins."
 
 
 
-TEXTE11= "j'aime le gucci mais aussi les grosses glaces. Je suis tombée ce matin sur mon nounourse.\
+TEXT11= "j'aime le gucci mais aussi les grosses glaces. Je suis tombée ce matin sur mon nounourse.\
         Il etait dans un état. je l'ai alors lavée. Maintenant il est tout propre. Je suis une fille"
 
 
+TEXT12 = " Les hommes embarquaient l'ancre. Je pris la barre et le bateau\
+pareil à un grand fantôme blanc glissa sur l'eau tranquille. Dès que nous fûmes dans la passe, le\
+yacht accéléra sa marche et sembla s'animer. Je fis route pour doubler le cap. Le jour naissait, les\
+étoiles s'éteignaient, le phare de Villefranche ferma pour la dernière fois son oeil tournant et\
+j'aperçus, dans le ciel lointain, au-dessus de Nice encore invisible, des lueurs roses : c'étaient les\
+glaciers des Alpes dont l'aurore allumait les cimes."
+
+TEXT13 = "Papa attrapait les souris toutes vivantes dans une espèce de boîte qui\
+se fermait sur elles en claquant et dont elles ne pouvaient sortir. J'allais les regarder là, par leur\
+petite fenêtre grillagée. Je voyais une jolie petite bête avec de jolis petits yeux vifs, et j'aurais bien\
+voulu la tenir dans ma main. Mais, dans la boîte, elle courait de tous les côtés pour trouver la porte,\
+de plus en plus folle de terreur."
+
+TEXT14 = " Isabelle jouait et des deux mains en même temps ! Les petits doigts\
+bruns couraient sur les touches, un mince bracelet d'argent dansait autour de son poignet. Parfois,\
+elle levait très haut une main qui restait suspendue en l'air une seconde puis retombait, avec une\
+vitesse incroyable, sur plusieurs notes à la fois, comme un épervier sur des hirondelles. Je ne\
+bougeais pas plus qu'une statue. Je regardais la crispation des fragiles épaules, et la petite nuque\
+pâle entre deux tresses de soie brillante."
 
 
+TEXT16 = "Tous les oiseaux aimaient la petite fille parce qu'elle était bonne pour eux. Ainsi un soir, elle put voir la belette qui rampait dans le sillon, vers le nid de l'alouette. Elle se mit aussitôt à crier, à taper dans ses mains, pour mettre en fuite la bête au poil jaune. Elle la chassa, la poursuivit au loin et les petites alouettes du nid furent sauvées. Un autre jour, elle sauva le rossignol que la grande couleuvre allait dévorer."
+
+TEXT17 = "je suis une fille et j'ai mangée une paprica au gout muscade."
+TEXT18 = ""
+TEXT19 = ""
+TEXT20 = ""
+TEXT21 = ""
+TEXT22 = ""
+TEXT23 = ""
+TEXT24 = ""
+TEXT25 = ""
+TEXT26 = ""
+TEXT27 = ""
+TEXT28 = ""
+TEXT29 = ""
+TEXT30 = ""
+TEXT31 = ""
+TEXT33 = ""
+TEXT34 = ""
+TEXT35 = ""
+TEXT36 = ""
+TEXT37 = ""
+TEXT38 = ""
+TEXT39 = ""
+TEXT40 = ""
+TEXT41 = ""
 
 
 
