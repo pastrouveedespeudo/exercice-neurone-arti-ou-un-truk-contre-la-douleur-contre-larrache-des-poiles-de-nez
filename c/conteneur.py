e362c470832bfaa756bf804be821c5d0d4461e50
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-07 17:09:41 +0200

    Update conteneur.py

--- c/conteneur.py
+++ c/conteneur.py
@@ -1,12 +1,15 @@
 
+import requests
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 
 PRONOMS = ["je","tu","il","nous","vous","ils"]
 
-FEMININ = ["lle","nne","tte","sse","ve","se","che","gne","ée",
-           "ées"]
+FEMININ = ["lle","nne","tte","sse","ve","se","che","gne","ée", "ées","e",
+           "ie","ée","ue"]
 
 IMPERATIF = ["e", "ons", "ds", "is","issons", "issez", "is", "ons"]
 
-        
+PONCTUATION = ['.',',',';','!','?',':']
 
 
