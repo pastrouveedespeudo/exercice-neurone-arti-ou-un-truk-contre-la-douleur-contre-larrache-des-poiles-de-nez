4a08c7844bee8acd452db4d30c24134e3e0e5584
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-07 14:08:09 +0200

    Update input.py

--- c/input.py
+++ c/input.py
@@ -55,13 +55,24 @@ class apprentissage1:
         self.texte = texte
         fem = False
         genre = {"feminin":0, "masculin":0}
+
+
         
         for i in self.texte:
             for j in i[0]:
-                for marque_feminin in FEMININ:
-                    nb = len(marque_feminin)
-                    if j[-nb:] == marque_feminin:
-                        fem = True
+                path = "https://www.le-dictionnaire.com/definition/{}"
+                path = path.format(j.lower())
+                requete = requests.get(path)
+                page = requete.content
+                soup = BeautifulSoup(page, "html.parser")      
+                propriete = soup.find("div", {"class":"defbox"})
+                adjectif = str(propriete).find("Adjectif")
+                if adjectif >= 0:
+                    for marque_feminin in FEMININ:
+                        nb = len(marque_feminin)
+                        if j[-nb:] == marque_feminin:
+                            fem = True
+
         if fem == True:
             genre["feminin"] += 1
         else:
@@ -78,12 +89,13 @@ if __name__ == "__main__":
 
 
     #entrée = input("texte ?")
-    liste = [TEXT, TEXT1, TEXT2, TEXT3, TEXT4, TEXT5,TEXT6,
-             TEXT7,TEXT8, TEXT9,TEXT10, TEXT11]
+    liste = [TEXT10, TEXT, TEXT1, TEXT2, TEXT3, TEXT4, TEXT5,TEXT6,
+             TEXT7,TEXT8, TEXT9, TEXT11]
     
     apprentissage1 = apprentissage1()
     
     for i in liste:
+    
         entrée = i
         entrée1 = "je suis très heureuse"
 
@@ -100,7 +112,7 @@ if __name__ == "__main__":
         genre = apprentissage1.opération_pour_genre(phrases)
         genre2 = apprentissage1.opération_pour_le_genre2(genre)
         apprentissage1.le_genre(genre2)
-        
+            
 
 
 
