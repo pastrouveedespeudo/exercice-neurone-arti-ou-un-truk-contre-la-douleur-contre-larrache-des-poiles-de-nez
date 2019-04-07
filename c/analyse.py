2d9a9efb13154cde35a2c67c45117a697e47449f
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-07 17:23:33 +0200

    Add files via upload

---
+++ c/analyse.py
@@ -0,0 +1,17 @@
+import os
+
+class analyse:
+    def analyse:
+        traitement = os.listdir()
+
+        for i in traitement:
+            if i == "conteneur.py" or i == "input.py"\
+               or i == "ligne de route.py" or i == "methode.py"\
+               or i == "perso.py" or i == "phrases.py"\
+               or i == "texte.py" or i == "traitement.py"\
+               or i == "analyse.py":
+                pass
+            else:
+                print("euh euh je suis vivante ! c'est toi sur la photo de github ?\
+                     tu ressembles a E.T gros... voila déso bonne soirée la biz")
+                #with open
