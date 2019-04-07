48140064f82094de28adc2c235bdbc580d11b4c1
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-07 13:51:02 +0200

    Update texte.py

--- c/texte.py
+++ c/texte.py
@@ -5,3 +5,135 @@ TEXT = "Cher Maxime, Je suis très heureux que tu viennes.\
         une lampe de poche, une boussole et un K-way, en cas de pluie.\
         Mes amitiés, Baptiste"
 
+TEXT1 = "Bonjour vesna, C'est très bien écrit, bravo ! Bonne soirée\
+        Cher Maxime,\
+        Je suis très heureux que tu viennes.\
+        Je suis sur  accent que nous allons faire de la marche en montagne.\
+        Mélanie vient avec nous dans les Vosges.\
+        Il faut que tu emportes des chaussures de\
+        marche, une lampe de poche, une boussole et un K-way, en cas de pluie.\
+        Mes amitiés,\
+        Baptiste"
+
+TEXT2 = "Salut Mehdi\
+        Je t'écris pour te dire que je suis à Paris depuis\
+        une semaine. Ici, il fait  un froid de canard et le soleil\
+        d'Agadir commence déjà à me manquer. Les études débutent\
+        bien et j'ai même fait la connaissance d'un nouvel ami qui\
+        s'appelle Julien. Il est très  sympa.\
+        Et toi comment vas-tu ? Tes études commencent-elles bien ?\
+        Et ta famille?\
+        Réponds–moi vite!\
+        À bientôt.\
+        Yassine"
+
+TEXTE3 = "Depuis deux mois, les petits renards sont nés. Alors, commence pour le\
+        père une vie dure et dangereuse. Au petit jour, il quitte sa retraite et passe la rivière pour venir rôder\
+        près d'une maison isolée, au milieu d'une sapinière. Caché dans les fougères, il attend patiemment le\
+        passage des volailles imprudentes."
+
+TEXTE4 =  "Élisabeth vit partir Jacques, les jambes raides, le dos rond, les coudes au corps,\
+            dans une attitude crispée. Il tomba dans un virage, se releva et continua sa descente plus lentement.\
+            Élisabeth était meilleure skieuse que lui. Elle s'engagea résolument dans le chemin poli et durci par\
+            des centaines de passages."
+
+
+
+TEXTE5 = "Je m'étais assis contre le mur de pierres sèches qui borde le pré. Le soleil de juillet\
+        tombant à pic m'écrasait d'une sorte de torpeur, lorsque je vis la vipère. Je ne l'avais pas entendue\
+        arriver. Elle avait dû longer le pied du mur et, rencontrant cet obstacle que je représentais pour elle,\
+        elle s'était arrêtée. Trouvant sans doute l'endroit propice à un bain de soleil, elle s'était lovée à\
+        quelques centimètres de moi et, sa tête camuse posée sur l'un de ses anneaux, elle me regardait des\
+        deux perles noires de ses yeux."
+
+
+
+TEXTE6 = "Je suis seule, lisant tes chères petites lettres avec les larmes aux\
+        yeux. Dans une quinzaine de jours, je vous reverrai, je vous embrasserai ; nous en aurons pour\
+        longtemps à être ensemble, et je serai bien heureuse. Continue d'être bonne et douce et de faire ma\
+        joie ; sois attentive et tendre avec ton excellente mère. Elle vous aime tant et est si digne d'être\
+        aimée ! Toutes les nuits, je regarde les étoiles comme nous faisions le soir sur le balcon et je pense à\
+        toi. Je vois avec plaisir que tu aimes et que tu comprends la nature. "
+
+
+TEXTE7 =  "J'ai retournée ma chaise et je l'ai placée comme celle du marchand de tabac\
+            parce que j'ai trouvée que c'était plus commode. J'ai fumée deux cigarettes, je suis rentré pour prendre\
+            un morceau de chocolat et je suis revenu le manger à la fenêtre. Peu après, le ciel s'est assombri et\
+            j'ai cru que nous allions avoir un orage. Il s'est découvert peu à peu cependant. Mais le passage des\
+            nuées avait laissé sur la rue comme une promesse de pluie... Je suis restée longtemps à regarder le\
+            ciel. À cinq heures, des tramways sont arrivés dans le bruit"
+
+
+TEXTE8 = "je suis une petite fille et j'adore les vetements. J'étais seule devant\
+        mon mirroir et je me vis si belle."
+
+TEXTE9 = "le soir quand je suis belle j'aime me levée et m'assoire"
+
+TEXTE10 = "Momo je t'embrasse. je suis contente de pouvoir t'ecrire au bordel\
+        que c'ets chiant. Mais je suis chiante. j'aime mon doudou et faire les magasins."
+
+
+
+TEXTE11= "j'aime le gucci mais aussi les grosses glaces. Je suis tombée ce matin sur mon nounourse.\
+        Il etait dans un état. je l'ai alors lavée. Maintenant il est tout propre. Je suis une fille"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
