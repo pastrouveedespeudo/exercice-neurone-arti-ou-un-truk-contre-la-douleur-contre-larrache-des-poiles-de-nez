c5fd0863aa7e52f43d6a4948f8b4529201d88261
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-07 14:34:30 +0200

    Add files via upload

---
+++ c/methode.py
@@ -0,0 +1,83 @@
+class ecriture:
+    
+    def ecriture1(self, carac):
+        self.carac = carac
+
+        with open("requete.py", "a") as file:
+
+            file.write(self.carac)
+            file.write("= [ ")
+
+
+    def ecriture3(self, carac):
+        self.carac = carac
+
+        with open("requete.py", "a") as file:
+
+            file.write()
+
+
+
+
+
+                       
+    def ecriture3(self, carac):
+        self.carac = carac
+
+        with open("requete.py", "a") as file:
+
+            file.write(" ]")
+                       
+
+class ecriture:
+    def analyse(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
