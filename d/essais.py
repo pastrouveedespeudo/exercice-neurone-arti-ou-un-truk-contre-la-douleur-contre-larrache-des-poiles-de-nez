adb485ff294ee934930666a565625ce30e78d4bf
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-13 23:47:21 +0200

    Add files via upload
    ta race les detection detecte pas le cercle, faudrai que jaille au centre et que je face moi meme le perimetre mais je fais ca comment
    .................

---
+++ d/essais.py
@@ -0,0 +1,186 @@
+import cv2
+import numpy as np
+from PIL import Image, ImageTk
+import argparse
+
+def a():
+    image = cv2.imread('Pikachu6.jpg')
+
+    compteur_rond_rouge = 0
+
+    liste_rond = []
+
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            if image[x,y][2] >= image[x,y][1] + 50 and\
+               image[x,y][2] >= 150 and\
+               image[x,y][1] <= 160 and\
+               image[x,y][1] >= 50 and\
+               image[x,y][0] >= 50 and\
+               image[x,y][0] <= 145 and\
+               image[x,y][2] >= image[x,y][0] + 50:
+                image[x,y] = 255,255,255
+                liste_rond.append((x,y))
+                compteur_rond_rouge += 1
+
+    print(compteur_rond_rouge)
+
+
+    
+ 
+    return liste_rond
+
+
+
+def c(liste_rond):
+
+
+    image2 = cv2.imread('rond.png')
+    
+    for i in liste_rond:
+        try:
+            image2[i[0], i[1]] = 0,0,0
+        except:
+            pass
+    cv2.imwrite("rond.png", image2)
+
+
+
+
+def reconnaissance_rond():
+
+    img = cv2.imread('rond.png',0)
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+
+
+
+
+
+
+
+
+
+
+
+reconnaissance_rond()
+
+
+def recolorie_image(self,image):
+    pass
+#en bleu sinon ca se retrecit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
