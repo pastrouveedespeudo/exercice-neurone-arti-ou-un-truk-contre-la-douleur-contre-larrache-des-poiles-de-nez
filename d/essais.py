123cdc38f60d625d27f3c46c442a93a6863ad7f6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-14 19:50:34 +0200

    Update essais.py

--- d/essais.py
+++ d/essais.py
@@ -10,7 +10,7 @@ import sys, cv2
 
 
 def extraction_rose():
-    image = cv2.imread('Pikachu7.png')
+    image = cv2.imread('Pikachu2.jpg')
 
     compteur_rond_rouge = 0
 
@@ -25,14 +25,15 @@ def extraction_rose():
                image[x,y][0] >= 50 and\
                image[x,y][0] <= 145 and\
                image[x,y][2] >= image[x,y][0] + 50 or\
-               image[x,y][2] >= image[x,y][0] + 100 and\
-               image[x,y][2] >= image[x,y][1] + 100 and\
-               image[x,y][1] <= 50 and image[x,y][0] <= 50 or\
-               image[x,y][2] >= 50 and image[x,y][0] == 0 and image[x,y][1] == 0 or\
-               image[x,y][2] >= image[x,y][0] + 150 and\
-               image[x,y][2] >= image[x,y][1] + 150 and\
-               image[x,y][0] <= 20 and image[x,y][1] <= 50:
-
+               image[x,y][2] >= 200 and\
+               image[x,y][0] <= 50 and\
+               image[x,y][1] <= 50 or\
+               image[x,y][2] >= 200 and\
+               image[x,y][1] <= 48 and\
+               image[x,y][0] <= 55 or\
+               image[x,y][2] >= 200 and\
+               image[x,y][1] <= 32 and\
+               image[x,y][0] <= 57:
                 image[x,y] = 255,255,255
                 liste_rond.append((x,y))
                 compteur_rond_rouge += 1
@@ -41,9 +42,16 @@ def extraction_rose():
     
     for i in liste_rond:
         image[i[0],i[1]] = 255,255,255
+
+
+
+
+
+    #image[300,380:400] = 0,255,0
+    
+
         
     cv2.imshow("output transform", image)
-    #cv2.imwrite('tyry.png', image)
     return liste_rond
 
 
@@ -65,32 +73,12 @@ def recon(liste):
 
 
   
-b = extraction_rose()
-#recon(b)
+extraction_rose()
+##recon(b)
 
 
 
-def recon2():
 
-
-    image = cv2.imread('Pikachu2.jpg')
-
-##    print(image[522,520]) 6
-##
-##    
-##    image[522,520:650] = 0,0,0
-##
-##
-##    print(image[300,380])
-##    image[300,380:650] = 0,0,0
-
-    print(image[155,140])
-    image[155,140:400] = 0,0,0
-    
-    cv2.imshow("output transform", image)
-    #cv2.imwrite("rond2.png", image)
-
-#recon2()
 
 
 
