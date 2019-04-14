0afa8bc263f10eaf216bcddc8dfc01a64ab98d46
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-14 19:28:09 +0200

    Update essais.py
    ya combien de teinte de rouge serieux ? .i.

--- d/essais.py
+++ d/essais.py
@@ -3,9 +3,14 @@ import numpy as np
 from PIL import Image, ImageTk
 import argparse
 import sys
+import imutils
+from pylab import *
+import sys, cv2
 
-def a():
-    image = cv2.imread('Pikachu6.jpg')
+
+
+def extraction_rose():
+    image = cv2.imread('Pikachu7.png')
 
     compteur_rond_rouge = 0
 
@@ -19,111 +24,73 @@ def a():
                image[x,y][1] >= 50 and\
                image[x,y][0] >= 50 and\
                image[x,y][0] <= 145 and\
-               image[x,y][2] >= image[x,y][0] + 50:
+               image[x,y][2] >= image[x,y][0] + 50 or\
+               image[x,y][2] >= image[x,y][0] + 100 and\
+               image[x,y][2] >= image[x,y][1] + 100 and\
+               image[x,y][1] <= 50 and image[x,y][0] <= 50 or\
+               image[x,y][2] >= 50 and image[x,y][0] == 0 and image[x,y][1] == 0 or\
+               image[x,y][2] >= image[x,y][0] + 150 and\
+               image[x,y][2] >= image[x,y][1] + 150 and\
+               image[x,y][0] <= 20 and image[x,y][1] <= 50:
+
                 image[x,y] = 255,255,255
                 liste_rond.append((x,y))
                 compteur_rond_rouge += 1
-
-    print(compteur_rond_rouge)
-
-
     
- 
+    print(compteur_rond_rouge)
+    
+    for i in liste_rond:
+        image[i[0],i[1]] = 255,255,255
+        
+    cv2.imshow("output transform", image)
+    #cv2.imwrite('tyry.png', image)
     return liste_rond
 
-b = a()
-
-def c(liste_rond):
-
-
-    print(liste_rond)
 
 
 
 
-c(b)
+def recon(liste):
 
+    print(liste)
+    image = cv2.imread('rond2.png')
 
-
-
-
-
-
-
-
-
-
-
-def reconnaissance_rond(liste):
-
-    image = cv2.imread('rond.png')
-
+    for i in liste:
+        image[i[0],i[1]] = 255,255,255
 
     cv2.imshow("output transform", image)
+    cv2.imwrite("rond2.png", image)
 
 
+
+
+  
+b = extraction_rose()
+#recon(b)
+
+
+
+def recon2():
+
+
+    image = cv2.imread('Pikachu2.jpg')
+
+##    print(image[522,520]) 6
+##
+##    
+##    image[522,520:650] = 0,0,0
+##
+##
+##    print(image[300,380])
+##    image[300,380:650] = 0,0,0
+
+    print(image[155,140])
+    image[155,140:400] = 0,0,0
     
+    cv2.imshow("output transform", image)
+    #cv2.imwrite("rond2.png", image)
 
-
-
-
-
-
-
-
-#reconnaissance_rond()
-
-
-def recolorie_image(self,image):
-    pass
-#en bleu sinon ca se retrecit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#recon2()
 
 
 
