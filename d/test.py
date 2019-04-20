3380abeee2ed37705b5c98f8c4f38d840f0767b3
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-20 22:55:34 +0200

    Update test.py
    ca prend pas l'oeil droit

--- d/test.py
+++ d/test.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from PIL import Image
-
+import shutil
 
 
 def refond_test_image(image, nom):
@@ -135,25 +135,78 @@ def yiyi(liste):
     for i in liste:
         img[i[0],i[1]] = 255,255,255
 
-        
+    print(liste)
+
+
+    cv2.imwrite('zazazazaz.png', img)
+    print('fin')
+
+    
+def superposition(image):
+
+    p1 = Image.open(image)
+    p2 = Image.open('zazazazaz.png')
+    p3 = Image.blend(p1, p2, 1)
+    p3.save('super.jpg')
+
+
+
+def pts_autour_yeux(image, liste):
+
+    img = cv2.imread(image,0)
+    imgg = cv2.imread(image)
+
+    path = r"C:\Users\jeanbaptiste\Desktop\reso\d\yeux"
+
+    h = 10
+    w = 15
+ 
+    print(liste)
+    
+    c = 0
+    
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            
+            for i in liste:
+
+                if x == i[0] and y == i[1]:
+                    crop_img = img[y-10:y+10+h, x-10:x+10+w]
+
+                    name = "crop" + str(c) + ".png"
+                    cv2.imwrite(name, crop_img)
+                    shutil.move(name, path)
+
+                          
+                    c+=1       
+ 
+                
+
+
+    #imgg[120:130, 116] = 0,0,255
+    
     cv2.startWindowThread()
     cv2.namedWindow("preview")
     cv2.imshow("preview", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    cv2.imwrite('zazazazaz.png', img)
-    print('fin')
-def superposition(image):
 
-    p1 = Image.open(image)
-    p2 = Image.open('zazazazaz.png')
-    p3 = Image.blend(p1, p2, .8)
-    p3.save('super.jpg')
+
+def detection_yeux():
+    pass
+
+def suppression_yeux():
+    pass
+
+
+
+
+
+
 
 
 #'eazeaze.png'
-    
 
 ##refond_test_image('zaeeee.png', 'zaeeee.png')
 ##
@@ -164,6 +217,6 @@ a = yuyu()
 yy('pikachu2.jpg')
 yiyi(a)
 superposition('pikachu2.jpg')
-
+pts_autour_yeux('pikachu2.jpg', a)
 
 
